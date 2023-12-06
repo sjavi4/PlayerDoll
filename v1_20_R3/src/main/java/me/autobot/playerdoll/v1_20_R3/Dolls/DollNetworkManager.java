f0f1f36b09e0f3bc6f5f306b8257d1b001ea6fd5
sjavi4 <72385460+sjavi4@users.noreply.github.com>
2023-12-06 13:58:00 +0800

    1.20.3

---
+++ v1_20_R3/src/main/java/me/autobot/playerdoll/v1_20_R3/Dolls/DollNetworkManager.java
@@ -0,0 +1,32 @@
+package me.autobot.playerdoll.v1_20_R3.Dolls;
+
+import io.netty.channel.embedded.EmbeddedChannel;
+import net.minecraft.network.Connection;
+import net.minecraft.network.PacketListener;
+import net.minecraft.network.protocol.PacketFlow;
+
+import java.net.SocketAddress;
+
+
+public class DollNetworkManager extends Connection {
+    public DollNetworkManager(PacketFlow enumprotocoldirection) {
+        super(enumprotocoldirection);
+        channel = new EmbeddedChannel();
+    }
+
+
+    @Override
+    public void setReadOnly() {}
+
+    @Override
+    public void handleDisconnection() {
+    }
+
+    @Override
+    public void setListener(PacketListener packetListener) {}
+
+    @Override
+    public SocketAddress getRemoteAddress() {
+        return this.address;
+    }
+}
