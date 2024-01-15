0915f2961bb34710c0971575b16f8fb05dce0eee
sjavi4 <72385460+sjavi4@users.noreply.github.com>
2024-01-15 14:02:47 +0800

    v1.20.2 [Fake player]

---
+++ core/src/main/java/me/autobot/playerdoll/Dolls/FakeChannel.java
@@ -0,0 +1,8 @@
+package me.autobot.playerdoll.Dolls;
+
+import io.netty.channel.embedded.EmbeddedChannel;
+
+public class FakeChannel extends EmbeddedChannel {
+    // Compatibility Adjustment
+    // Pass Fake player check
+}
