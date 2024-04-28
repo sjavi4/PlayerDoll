7640894c80b8ba7a0d6d9b1a492e795fae028a14
sjavi4 <72385460+sjavi4@users.noreply.github.com>
2024-04-28 19:07:11 +0800

    Delete utility directory

--- utility/src/main/java/me/autobot/playerdoll/Util/PermChecker.java
+++
@@ -1,8 +0,0 @@
-package me.autobot.playerdoll.Util;
-
-import me.autobot.playerdoll.Util.Configs.PermConfig;
-
-@FunctionalInterface
-public interface PermChecker {
-    boolean check(PermConfig config);
-}
