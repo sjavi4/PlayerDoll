2eb7e732ff2573501fbc0e40df6cf0d92185b686
sjavi4 <72385460+sjavi4@users.noreply.github.com>
2024-12-08 00:49:24 +0800

    version 31

--- PlayerDoll-API/src/main/java/me/autobot/playerdoll/api/config/AbstractConfig.java
+++ PlayerDoll-API/src/main/java/me/autobot/playerdoll/api/config/AbstractConfig.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.logging.Level;
 
 public abstract class AbstractConfig {
-    public static final int VERSION = 30;
+    public static final int VERSION = 31;
     private final boolean checkUpdate;
     private final File yamlFile;
     private YamlConfiguration yamlConfiguration;
