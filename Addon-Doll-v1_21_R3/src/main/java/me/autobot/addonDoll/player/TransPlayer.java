5732fb0c9eab92e09d229197d8d37d27f103b3b8
sjavi4 <72385460+sjavi4@users.noreply.github.com>
2024-12-11 17:33:29 +0800

    Pull Method to upper class

--- Addon-Doll-v1_21_R3/src/main/java/me/autobot/addonDoll/player/TransPlayer.java
+++ Addon-Doll-v1_21_R3/src/main/java/me/autobot/addonDoll/player/TransPlayer.java
@@ -1,6 +1,5 @@
 package me.autobot.addonDoll.player;
 
-import me.autobot.playerdoll.api.ReflectionUtil;
 import me.autobot.playerdoll.api.doll.DollStorage;
 import me.autobot.playerdoll.api.doll.ExtendPlayer;
 import net.minecraft.server.level.ServerPlayer;
@@ -8,9 +7,8 @@ import net.minecraft.server.level.ServerPlayer;
 public class TransPlayer extends ExtServerPlayer implements ExtendPlayer {
 
     public TransPlayer(ServerPlayer serverPlayer) {
-        super(serverPlayer.server, serverPlayer.serverLevel(), serverPlayer.getGameProfile());
+        super(serverPlayer.server, serverPlayer.serverLevel(), serverPlayer.getGameProfile(), serverPlayer);
         DollStorage.ONLINE_TRANSFORMS.put(serverPlayer.getUUID(), this);
-        ReflectionUtil.setConvertPlayerPermBase(ReflectionUtil.NMSToBukkitPlayer(serverPlayer), getBukkitPlayer());
     }
 
     @Override
